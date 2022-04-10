7be1d5e14b587c94f6395add4b866d6513690d6e
Bagus Widiyanto <bgswdyt@gmail.com>
2022-04-10 09:54:44 +0700

    remove debug banner and updated readme

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter App'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
